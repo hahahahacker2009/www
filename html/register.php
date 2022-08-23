27b57012142f53fc90d0f1ae7119a2678c88d8e5
Someone <hahahahacker2009@outlook.com>
2022-08-23 14:22:54 +0700

    fix error when checking exists, stick forms

--- html/register.php
+++ html/register.php
@@ -18,21 +18,21 @@
 		require_once("../mysqli_connect.php");
 		$msg = NULL;
 		if (!empty($_POST['disp_name'])) {
-			$disp_name = escape_data($_POST['disp_name']);
+			$disp_name = escape_data_in($_POST['disp_name']);
 		} else {
 			$disp_name = FALSE;
 			$msg .= "Vui long nhap ten hien thi! <br />";
 		}
 
 		if (!empty($_POST['username'])) {
-			$username = escape_data($_POST['username']);
+			$username = escape_data_in($_POST['username']);
 		} else {
 			$username = FALSE;
 			$msg .= "Vui long nhap ten dang nhap! <br />";
 		}
 
 		if (!empty($_POST['email'])) {
-			$email = escape_data($_POST['email']);
+			$email = escape_data_in($_POST['email']);
 		} else {
 			$email = FALSE;
 			$msg .= "Vui long nhap email cua ban! <br />";
@@ -41,7 +41,7 @@
 		if (!empty($_POST['password'])) {
 			if ($_POST['password'] == $_POST['verify']) {
 				if (strlen($_POST['password']) >= 8) {
-					$password = escape_data($_POST['password']); 
+					$password = escape_data_in($_POST['password']); 
 				} else {
 					$password = FALSE;
 					$msg .= "Mat khau phai co do dai tu 8 den 48 ki tu! <br />";
@@ -56,30 +56,8 @@
 		}
 
 		if ($disp_name && $username && $email && $password) {
-			function check_username($username, $dbc) {
-				$query = "SELECT user_id FROM user WHERE username='$username'";
-				$result = @mysqli_query($dbc, $query);
-
-				if (mysqli_num_rows($result) == 0) {
-					return FALSE;
-				} else {
-					return TRUE;
-				}
-			}
-
-			function check_email($email, $dbc) {
-				$query = "SELECT user_id FROM user WHERE email='$email'";
-				$result = @mysqli_query($dbc, $query);
-
-				if (mysqli_num_rows($result) == 0) {
-					return FALSE;
-				} else {
-					return TRUE;
-				}
-			}
-
-			echo $exist['username'];
-			echo $exist['email'];
+			$exist['username'] = check_username($username, $dbc);
+			$exist['email'] = check_email($email, $dbc);
 
 			if ($exist['username'] == FALSE && $exist['email'] == FALSE) {
 				$query = "INSERT INTO user (disp_name, username, email, password, registration_date) VALUES ('$disp_name', '$username', '$email', SHA2('$password', 256), NOW());";
@@ -105,7 +83,8 @@
 					$msg .= 'Email da duoc su dung. <br />';
 				}
 			}
-		mysqli_close($dbc);
+			mysqli_close($dbc);
+
 		} else {
 			$msg .= "Vui long thu lai!";
 		}
@@ -122,15 +101,15 @@
 	<legend><h3>Nhap vao thong tin cua ban:</h3></legend>
 	<tr>
 		<td width="25%">&nbsp</td>
-		<td width="20%"><b>Ten hien thi:</b></td> <td><input type="text" name="disp_name" size="20" maxlength="40" placeholder="Ten hien thi" required="required" /></td>
+		<td width="20%"><b>Ten hien thi:</b></td> <td><input type="text" name="disp_name" size="20" maxlength="40" placeholder="Ten hien thi" value="<?php if(isset($_POST['disp_name'])) {echo stripslashes($_POST['disp_name']);} ?>" required="required" /></td>
 	</tr>
 	<tr>
 		<td width="25%">&nbsp</td>
-		<td width="20%"><b>Ten dang nhap:</b></td> <td><input type="text" name="username" size="20" maxlength="30" placeholder="Ten dang nhap" required="required" /></td>
+		<td width="20%"><b>Ten dang nhap:</b></td> <td><input type="text" name="username" size="20" maxlength="30" placeholder="Ten dang nhap" value="<?php if(isset($_POST['username'])) {echo stripslashes($_POST['username']);} ?>" required="required" /></td>
 	</tr>
 	<tr>	
 		<td width="25%">&nbsp</td>
-		<td width="20%"><b>Dia chi email:</b></td> <td><input type="email" name="email" size="30" maxlength="60" placeholder="Email" required="required" /></td>
+		<td width="20%"><b>Dia chi email:</b></td> <td><input type="email" name="email" size="30" maxlength="60" placeholder="Email" value="<?php if(isset($_POST['email'])) {echo stripslashes($_POST['email']);} ?>" required="required" /></td>
 	</tr>
 	<tr>
 		<td width="25%">&nbsp</td>
