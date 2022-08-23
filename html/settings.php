e4c8a2be05b64c70203d370bcbc5c563c320c8ad
Someone <hahahahacker2009@outlook.com>
2022-08-23 17:53:51 +0700

    requiring current password to change information, change session data when change username and display name, bug fixes

--- html/settings.php
+++ html/settings.php
@@ -11,8 +11,8 @@
 
 <?php
 	if (isset($_SESSION['loggedin'])) {
-
 		require_once("../mysqli_connect.php");
+		$msg = NULL;
 		$query = "SELECT user_id, disp_name, username, email, registration_date FROM user WHERE username='{$_SESSION['username']}'";
 		$result = @mysqli_query($dbc, $query);
 		$assoc = mysqli_fetch_assoc($result);
@@ -22,44 +22,64 @@
 		}
 
 		if (isset($_POST['update'])) {
-			if (!empty($_POST['disp_name'])) {
-				if ($_POST['disp_name'] != $assoc['username']) {
-					$disp_name = escape_data_in($_POST['disp_name']);
-					$query = "UPDATE user SET disp_name = '$disp_name' WHERE user_id='$userid' LIMIT 1";
-					$result = @mysqli_query($dbc, $query);
-					if (mysqli_affected_rows($dbc) == 1) {
-						echo 'Ten hien thi cua ban da duoc thay doi.';
-					} else {
-						echo 'Da gap loi khi thay doi ten hien thi cua ban!';
+			if (!empty($_POST['auth'])) {
+				$query = "SELECT user_id FROM user WHERE username='{$assoc['username']}' AND password=SHA2('{$_POST['auth']}', 256)";
+				echo "Cau truy van se duoc thuc hien: $query";
+				$result = @mysqli_query($dbc, $query);
+				$num = mysqli_num_rows($result);
+				if ($num == 1) {
+					if (!empty($_POST['disp_name'])) {
+						if ($_POST['disp_name'] != $assoc['disp_name']) {
+							$disp_name = escape_data_in($_POST['disp_name']);
+							$query = "UPDATE user SET disp_name = '$disp_name' WHERE user_id='$userid' LIMIT 1";
+							$result = @mysqli_query($dbc, $query);
+							if (mysqli_affected_rows($dbc) == 1) {
+								$msg .= 'Ten hien thi cua ban da duoc thay doi. <br />';
+								$_SESSION['disp_name'] = $disp_name;
+							} else {
+								$msg .= 'Da gap loi khi thay doi ten hien thi cua ban! <br />';
+							}
+						}
 					}
-				}
-			}
 
-			if (!empty($_POST['username'])) {
-				if ($_POST['username'] != $assoc['username']) {
-					$username = escape_data_in($_POST['username']);
-					$query = "UPDATE user SET username = '$username' WHERE user_id='$userid' LIMIT 1";
-					$result = @mysqli_query($dbc, $query);
-					if (mysqli_affected_rows($dbc) == 1) {
-						echo 'Ten dang nhap cua ban da duoc thay doi. <br />';
-					} else {
-						echo 'Da gap loi khi thay doi ten dang nhap cua ban! <br />';
+					if (!empty($_POST['username'])) {
+						if ($_POST['username'] != $assoc['username']) {
+							$username = escape_data_in($_POST['username']);
+							$query = "UPDATE user SET username = '$username' WHERE user_id='$userid' LIMIT 1";
+							$result = @mysqli_query($dbc, $query);
+							if (mysqli_affected_rows($dbc) == 1) {
+								$msg .= 'Ten dang nhap cua ban da duoc thay doi. <br />';
+								$_SESSION['username'] = $username;
+							} else {
+								$msg .= 'Da gap loi khi thay doi ten dang nhap cua ban! <br />';
+							}
+						}
 					}
-				}
-			}
 
-			if (!empty($_POST['email'])) {
-				if ($_POST['email'] != $assoc['email']) {
-					$email = escape_data_in($_POST['email']);
-					$query = "UPDATE user SET email = '$email' WHERE user_id='$userid' LIMIT 1";
-					$result = @mysqli_query($dbc, $query);
-					if (mysqli_affected_rows($dbc) == 1) {
-						echo 'Email cua ban da duoc thay doi. <br />';
-					} else {
-						echo 'Da gap loi khi thay doi email cua ban! <br />';
+					if (!empty($_POST['email'])) {
+						if ($_POST['email'] != $assoc['email']) {
+							$email = escape_data_in($_POST['email']);
+							$query = "UPDATE user SET email = '$email' WHERE user_id='$userid' LIMIT 1";
+							$result = @mysqli_query($dbc, $query);
+							if (mysqli_affected_rows($dbc) == 1) {
+								$msg .= 'Email cua ban da duoc thay doi. <br />';
+							} else {
+								$msg .= 'Da gap loi khi thay doi email cua ban! <br />';
+							}
+						}
 					}
+				} else {
+					$msg .= "Mat khau hien tai khong dung! <br />";
 				}
+			} else {
+				$msg .= "Truoc khi thay doi thong tin, NHAP VAO MAT KHAU CUA BAN! <br />";
 			}
+		}
+
+		mysqli_close($dbc);
+
+		if (isset($msg)) {
+			echo "<p style=\"color: red;\">{$msg} </p>";
 		}
 
 	} else {
@@ -74,6 +94,10 @@
 <fieldset>
 <table border="0" width="100%" align="center">
 	<legend><h3>Thong tin cua ban:</h3></legend>
+	<tr>
+		<td width="25%">&nbsp</td>
+		<td width="20%"><b>Mat khau hien tai:</b></td> <td><input type="password" name="auth" size="20" maxlength="40" placeholder="Mat khau hien tai" required="required" /></td>
+	</tr>
 	<tr>
 		<td width="25%">&nbsp</td>
 		<td width="20%"><b>Ten hien thi:</b></td> <td><input type="text" name="disp_name" size="20" maxlength="40" placeholder="<?php echo $assoc['disp_name']; ?>" /></td>
