c8b0991982d19605dca27689f57bebf33505cf99
Someone <hahahahacker2009@outlook.com>
2022-08-23 14:20:40 +0700

    put check username and email functions in; change escape_data() function name

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -7,7 +7,33 @@ DEFINE ('DB_NAME', 'apache2');
 $dbc = mysqli_connect(DB_HOST, DB_ID, DB_PW) OR die("Khong the thiet lap ket noi den co so du lieu!");
 mysqli_select_db($dbc, DB_NAME) OR die("Khong the chon co so du lieu!");
 
-function escape_data($data, $trim=TRUE) {
+function check_username($username, $dbc) {
+	$query = "SELECT user_id FROM user WHERE username='$username'";
+	$result = @mysqli_query($dbc, $query);
+
+	if (mysqli_num_rows($result) == 0) {
+		return FALSE;
+		mysqli_free_result($dbc, $result);
+	} else {
+		return TRUE;
+		mysqli_free_result($dbc, $result);
+	}
+}
+
+function check_email($email, $dbc) {
+	$query = "SELECT user_id FROM user WHERE email='$email'";
+	$result = @mysqli_query($dbc, $query);
+
+	if (mysqli_num_rows($result) == 0) {
+		return FALSE;
+		mysqli_free_result($dbc, $result);
+	} else {
+		return TRUE;
+		mysqli_free_result($dbc, $result);
+	}
+}
+
+function escape_data_in($data, $trim=TRUE) {
 	global $dbc;
 	if (ini_get('magic_quotes_gpc')) {
 		$data = stripslashes($data);
