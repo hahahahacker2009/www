39fab53ae21aebfb0079da543a519633fe3cfbe7
Someone <hahahahacker2009@outlook.com>
2022-08-24 14:32:08 +0700

    search

--- html/profiles.php
+++ html/profiles.php
@@ -8,24 +8,26 @@
 
 <h1>Ho so nguoi dung</h1>
 <?php
-	if (isset($_GET['username'])) {
-		require_once("../mysqli_connect.php");
-		$username = escape_data_in($_GET['username']);
-		$query = "SELECT disp_name, username, email, registration_date FROM user WHERE username='{$username}'";
-		echo "Cau truy van se duoc thuc hien: $query";
-		echo "<br /><br /><br /><br />";
-		$result = @mysqli_query($dbc, $query);
-		$assoc = mysqli_fetch_assoc($result);
+	if (isset($_GET['search'])) {
+		if (!empty($_GET['username'])) {
+			require_once("../mysqli_connect.php");
+			$username = escape_data_in($_GET['username']);
+			$query = "SELECT disp_name, username, email, registration_date FROM user WHERE username='{$username}'";
+			//echo "Cau truy van se duoc thuc hien: $query";
+			echo "<br /><br /><br /><br />";
+			$result = @mysqli_query($dbc, $query);
+			$assoc = mysqli_fetch_assoc($result);
 
-		if ($assoc) {
-			echo "<h2>{$assoc['disp_name']} ({$assoc['username']})</h2>";
-			echo "<h3>Ngay dang ki: {$assoc['registration_date']} (YYYY-MM-DD)</h3>";
-			echo "<h4>Email nguoi dung: {$assoc['email']}</h4>";
+			if ($assoc) {
+				echo "<h2>{$assoc['disp_name']} ({$assoc['username']})</h2>";
+				echo "<h3>Ngay dang ki: {$assoc['registration_date']} (YYYY-MM-DD)</h3>";
+				echo "<h4>Email nguoi dung: {$assoc['email']}</h4>";
+			} else {
+				echo "<h2 style=\"color: red;\">Khong tim thay nguoi dung!</h2>";
+			}
 		} else {
-			echo "<h2 style=\"color: red;\">Khong tim thay nguoi dung!</h2>";
+			echo "<h3 style=\"color: red;\">Ten nguoi dung chua duoc chi dinh!</h3>";
 		}
-	} else {
-		echo "<h3 style=\"color: red;\">Ten nguoi dung chua duoc chi dinh!</h3>";
 	}
 ?>
 
