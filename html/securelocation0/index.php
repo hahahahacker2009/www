bfd5edcd532b048991465ed403ceda5f0288e938
Someone <hahahahacker2009@outlook.com>
2022-08-25 17:45:36 +0700

    using auth.php and display phpinfo

--- html/securelocation0/index.php
+++ html/securelocation0/index.php
@@ -9,18 +9,9 @@ include("./include/header.html");
 <p>Quan tri website dong cua ban</p>
 <?php
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/include/config.php");
-	if (isset($_SESSION['loggedin'])) {
-		if (isset($_SESSION['role'])) {
-			if (isset($_SESSION['username'])) {
-				echo "<p>Ban dang dang nhap voi ten nguoi dung la {$_SESSION['username']}, vai tro quan tri cua ban la {$_SESSION['role']} ({$_SESSION['role_id']})</p>";
-			}
-		} else {
-			echo "<p> Tai khoan cua ban khong co vai tro quan tri! </p>";
-		}
-	} else {
-		header("Location: http://{$_SERVER['HTTP_HOST']}/index.php");
-		exit();
-	}
+	require_once("./auth.php");
+
+	phpinfo();
 ?>
 
 
