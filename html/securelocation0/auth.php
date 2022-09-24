08be420ed9445649bbec37eef8b5a0f764fcabd1
Someone <hahahahacker2009@outlook.com>
2022-09-24 17:04:36 +0700

    unnecessary code

--- html/securelocation0/auth.php
+++ html/securelocation0/auth.php
@@ -1,6 +1,4 @@
 <!-- BAT DAU NOI DUNG TRANG -->
-<h1>Trang chu quan tri</h1>
-<h2>Chao mung ban den voi trang chu quan tri cua website!</h2>
 <p>Quan tri website dong cua ban</p>
 <?php
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/include/config.php");
